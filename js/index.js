eb8206b24c8dc179940a1769ca3a8c9cc2dd0862
Volkersen <123008787+andreirusen@users.noreply.github.com>
2024-01-04 20:40:06 +0200

    update : maps, abonamente, footer

--- js/index.js
+++ js/index.js
@@ -99,20 +99,3 @@ console.clear();
     sessionStorage.setItem("offcanvasShown", true);
   }
 })();
-
-// Google Map
-let map;
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 44.434831044470364, lng: 26.147003026598945 },
-    zoom: 15,
-  });
-  new google.maps.Marker({
-    position: { lat: 44.434831044470364, lng: 26.147003026598945 },
-    map: map,
-    title: "RA Athletic",
-    Animation: google.maps.Animation.BOUNCE,
-  });
-}
-initMap();
